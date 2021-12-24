e70f2bd106b7db780dd30fbc1008c460b8388159
IlyaGoncharovY <goncharov.i@rambler.ru>
2021-12-24 22:51:05 +0300

    test OK

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function missingNo(nums) {
-    // You are given an unsorted array containing
-    //  all the integers from 0 to 100 inclusively. 
-    //  However, one number is missing. Write a function 
-    //  to find and return this number. What are the time 
-    //  and space complexities of your solution?
-  }
+    for (let i; i < nums.length; i++) {
+        if (!nums.includes(i)) {
+            return i;
+        }
+    }
+}
